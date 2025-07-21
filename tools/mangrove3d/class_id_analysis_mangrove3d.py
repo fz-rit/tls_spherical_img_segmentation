f4da9713eaeaebff4eb71628be2deabf049ef6fd
fz-rit <fzhcis@rit.edu>
2025-07-21 16:47:10 -0400

    Update label file count check in class ID analysis to expect 30 files

--- tools/mangrove3d/class_id_analysis_mangrove3d.py
+++ tools/mangrove3d/class_id_analysis_mangrove3d.py
@@ -116,8 +116,8 @@ def plot_class_id_hist(class_id_vec, class_map, save_path):
 
 root_dir = Path("/home/fzhcis/data/mangrove3d_pcd/train_val/")
 label_paths = list(root_dir.glob('**/*_refined.label'))
-if len(label_paths) !=39:
-    raise ValueError(f"Expected 39 label files, found {len(label_paths)}. Please check the directory structure.")
+if len(label_paths) !=30:
+    raise ValueError(f"Expected 30 label files, found {len(label_paths)}. Please check the directory structure.")
 class_id_ls, class_id_all = grab_class_id(label_paths)
 save_path = root_dir / "class_id_histogram.png"
 plot_class_id_hist(class_id_all, CLASS_MAP, save_path=save_path)
